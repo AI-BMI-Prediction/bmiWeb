b7e1b900045d2b54a7c0823c68e62a591e12dd7a
maatanyy <maatanyy@naver.com>
2022-12-02 00:58:01 +0900

    first init add home app

---
+++ explain.py
@@ -0,0 +1,15 @@
+
+
+### 시작법
+# cd bmiproject  한 후 python manage.py runserver 치면 됨
+
+
+### 나오는 법
+# ctrl + c
+
+### app만드는 법
+# django startapp [앱이름]
+# app 만든 후에는 settings.py installed app에 추가해줘야함
+
+###
+# url, view가 웹 이어주는 파트고, model은 아직 비어있는데 여기에 model (class) 같은 거 생성하는 db 관련 파트라고 생각하면 편함

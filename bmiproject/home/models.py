c3f498143e20fca3f204070018ec2746013f1e94
maatanyy <maatanyy@naver.com>
2022-12-08 21:19:11 +0900

    add db

--- bmiproject/home/models.py
+++ bmiproject/home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=10)
+    weight = models.FloatField()
+    height = models.FloatField()
+    step = models.FloatField()
+    burn = models.FloatField()
+    eat = models.FloatField()
+    sleep = models.FloatField()
+    label = models.IntegerField(default=None)
+    predict_label = models.IntegerField(default=None)

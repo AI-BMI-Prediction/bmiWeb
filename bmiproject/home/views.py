6f3dce3c76be64ddd884970228dedb6a26a06b72
Evergyu <Evergyu@github.com>
2022-12-08 21:18:06 +0900

    html template

--- bmiproject/home/views.py
+++ bmiproject/home/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def result(request):
+    name = request.POST['username']
+    students = ['inu', 'james', 'bob']
+
+    if name in students:
+        is_exist = True
+    else:
+        is_exist = False
+
+    return render(request, 'result.html', {'user_name': name, 'is_exist':is_exist})
